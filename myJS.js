8f4a90b7f10a3c3b645d30be48f28831d471557b
CHIA-WEI Liu <fattyboobootw@gmail.com>
2019-02-22 20:37:26 +0800

    update

--- myJS.js
+++ myJS.js
@@ -1,10 +1,10 @@
 
-var firstUrl = 'https://uniquetoone.com/index/index/a1f6c42d-ab0e-4903-b6e6-72058e899e23';//ip-azure
-var secondUrl = 'https://app-versions.herokuapp.com/index/index/f8d1d0e0-b1d7-4892-b960-62f907cdeaae';//heroku
+var firstUrl = 'https://uniquetoone.com/index/index/f21957ba-03e0-42de-a3ff-9efe122327b1';//ip-azure
+var secondUrl = 'https://app-versions.herokuapp.com/index/index/6b7b1e0a-d8b4-49c9-97e7-21a6ee0bb51f';//heroku
 
 function getAnimation(){
     // animation data.json URL
-    let commentUulJson = "https://nininenine.github.io/animation_oneMax/data.json";
+    let commentUulJson = " https://nininenine.github.io/sub149/data.json";
 
     // amimation action
     var animation = bodymovin.loadAnimation({
@@ -32,7 +32,7 @@ function getAjax2(){
       if (response.version == 1) {
         getAnimation();
          $('#lottie_animation > svg').css('border-radius','25px');
-          
+
         return false;
       }
       new Vue({
@@ -99,7 +99,7 @@ function getAjax2(){
     alert("發生錯誤: " + xhr.status + " " + xhr.statusText);
     }
 
-    
+
   });
 }
 function getAjax1(){
